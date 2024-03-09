10ec3e728d5e2e8d041ed91b1bab7da06282189e
Mohamed Fazil M <fazilmm860@gmail.com>
2024-03-09 15:19:09 +0530

    edit as per client request

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,12 +1,10 @@
 function SendMail(){
     var params={
-      from_name : document.getElementById("firstName").value,
-      lastName:document.getElementById("lastName").value,
+      from_name : document.getElementById("custName").value,
       companyName:document.getElementById("companyName").value,
-      bussinessEmail:document.getElementById("businessEmail").value,
-      jobTitle:document.getElementById("jobTitle").value,
+      businessEmail:document.getElementById("businessEmail").value,
       industry:document.getElementById("industry").value,
-      country:document.getElementById("country").value,
+      custNumber:document.getElementById("custNumber").value,
       queries:document.getElementById("queries").value
     }
     emailjs.send("service_85j8xgp","template_gtaiev6",params).then(function(res){
