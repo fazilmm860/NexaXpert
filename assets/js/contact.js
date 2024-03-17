ced055b6ff0dc60df764e20bcf3d6aa015dd09b8
Mohamed Fazil M <fazilmm860@gmail.com>
2024-03-17 13:31:45 +0530

    edit in contact page

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -3,7 +3,6 @@ function SendMail(){
       from_name : document.getElementById("custName").value,
       companyName:document.getElementById("companyName").value,
       businessEmail:document.getElementById("businessEmail").value,
-      industry:document.getElementById("industry").value,
       custNumber:document.getElementById("custNumber").value,
       queries:document.getElementById("queries").value
     }
